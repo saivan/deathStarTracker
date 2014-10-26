1b83c303b436928e22af4e2fd3b797f2df253ebb
mRowlinson27 <michaelrowlinson27@gmail.com>
2014-10-26 16:23:43 +1100

    Updated the ultrasound c and header files
    Still untested with the actual ultrasound, note still has clock times
    set for the 4MHz chip, has comments on where it is needed to be changed
    and what to

--- Modules/Ultrasound/ultrasound.c
+++ Modules/Ultrasound/ultrasound.c
@@ -1,3 +1,4 @@
+
 /*
 ;**********************************************************
 ;*  proj_ultrasound.c
@@ -6,182 +7,194 @@
 ;*  29 September 2014
 ;*  Assembled with MPLAB X IDE v2.15
 ;**********************************************************
-;*  Pin RC1 used to initilize echos
-;*  Pin RB5 used to recieve the echo
+;*  Pin RC2 used to initilize echos
+;*  Pin RC1 used to recieve the echo THIS MUST BE SELECTED IN CCP2MUX
 ;*  Timer 0 used in capture mode on the rising edge of echo
 ;**********************************************************
  */
 
-#include <p18cxxx.h>
-#include <timers.h>
-//#include "ConfigRegs18f4520.h"
-#include <p18f452.h>
-#include "realTimer.h"
-#include "LCD.h"
+#include "masterHeader.h"
 
 
-//#define     INITPIN     PORTCbits.RC1
-//#define     INITTRIS    TRISCbits.RC1
-#define     INITPIN     PORTBbits.RB2
-#define     INITTRIS    TRISBbits.RB2
-#define     ECHOPIN     PORTBbits.RB1
-#define     ECHOTRIS    TRISBbits.RB1
-#define     OVERFCONST  0xFF
-
-unsigned int  TMR0StartMilli;
-unsigned int  TMR0StartSubMilli;
-unsigned int  TMR0EndMilli;
-unsigned int  TMR0EndSubMilli;
-unsigned int  milliTime;
-unsigned int  subMilliTime;
-unsigned char done;
-unsigned char i;
-unsigned int distance;
-unsigned int speed;
-unsigned int distPerMs;
-unsigned int distPerSubMs;
-timeTag echoStartTime = {0,0,0,0};
-timeTag echoEndTime = {0,0,0,0};
-timeTag echo = {0,0,0,0};
-unsigned char test;
+/// The external variables are all declared here
 
 
-void high_interrupt(void);
-void highPriorityIsr(void);
-void fireEcho (void);
-void echoCalc (void);
-void low_interrupt(void);
-
-#pragma code lowerPriorityInterruptAddress=0x0008
-void low_interrupt(void) {
-    _asm GOTO highPriorityIsr _endasm
-}
-
-#pragma code highPriorityInterruptAddress=0x0018
-void high_interrupt(void) {
-    _asm GOTO highPriorityIsr _endasm
-}
+unsigned char USArrayPosition = 0;
+unsigned int speedOfSound = 0;
+unsigned int distPerMs = 0;
+unsigned int distPerSubMs = 0;
+unsigned int UScapturedValue[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+timeTag echoCanFire = {0,0,0,0};
+USFlagType USFlags = {0, 0};
+USValueType USValues = {0,0,0,0,0};
 
 
-void setup(void) {
+
+/**
+ * @brief [sets up the US]
+ * @details [configures timer 3 and sets initial status of
+ *			 US variables]
+ */
+void USSetup(void) {
     INITPIN = 0;
     ECHOTRIS = 1;                   ///<Input for Echo
     INITTRIS = 0;                   ///<Output for INIT
     INTCONbits.GIE_GIEH = 1;        ///<Turn on interrupts
     INTCONbits.PEIE_GIEL = 1;
-    INTCON3bits.INT1IE = 1;
-    INTCON2bits.INTEDG2 = 1;
-    INTCON3bits.INT1IP = 1;
 
-    TMR0StartMilli = 0;
-    TMR0EndMilli = 0;
-    TMR0StartSubMilli = 0;
-    TMR0EndSubMilli = 0;
-    milliTime = 0;
-    subMilliTime = 0;
-    distance = 0;
+    PIR2bits.CCP2IF = 0;
+    CCP2CON = 0x00; ///<capture mode, every rising edge
+    T3CONbits.RD16 = 0; ///<set to 8 bit mode
+    T3CONbits.T3CCP2 = 0;
+    T3CONbits.T3CCP1 = 1; ///<timer 3 used for CCP2
+    T3CONbits.T3CKPS1 = 0;
+    T3CONbits.T3CKPS0 = 0; ///<prescaler is 1:1
+    T3CONbits.TMR3ON = 1;
+
+    USValues.distance = 0;                         ///<holds current value of calc distance
     setupRealTimeTimer();
-    speed = 34000;
-    distPerMs = speed/2000;
-    distPerSubMs = speed/16000;
+    speedOfSound = 34000;                          ///<speed in cm/s
+    distPerMs = speedOfSound/200;                  ///<divides by 2 for travel, 100 to be mm/ms
+    distPerSubMs = speedOfSound/4000;              ///<divides by 20 times to get 20 subsections
+    USFlags.fireStatus = 0;                       ///<status of 0 means nothing is occuring, 1 means echo is incoming and has not been calculated yet
+    USFlags.distanceReady = 0;                    ///<whether the new distance has been read
+    USValues.sampleSize = 2;                       ///<starting values
+    USValues.minRange = 450;
+    USValues.maxRange = 2100;
+    USValues.freq_ms = 80;
+    USArrayPosition = 0;
 }
 
-void main(void) {
+/**
+ * @brief [sets up the US Min Distance]
+ */
+void setUSMin (unsigned int min){
+    USValues.minRange = min;
+}
 
-    setup();
-    LCDInitialise();
-    test = 1;
-    while (test != 0) //delay for device to ready
-    {
-        test++;
+/**
+ * @brief [sets up the US Max Distance]
+ */
+void setUSMax (unsigned int max){
+    USValues.maxRange = max;
+}
+
+/**
+ * @brief [sets up the US Samples per Average]
+ */
+void setUSSamplePerEstimate (unsigned char samples){
+    unsigned char i = 0;
+    while (i < 10) {
+        UScapturedValue[i] = 0;
+        i++;
     }
-    fireEcho();
-    done = 2;
-    while (1)
-    {
-//        char string[] =  "bah humbug";
-//        updateTime();
-//        intToDisplay(0);
-//        LCDMoveCursor(0,0);
-//        delayMs(1);
-//        LCDWriteHere( string );
-//        delayMs(1);
-//        intToDisplay(distPerMs);
-//        LCDMoveCursor(1,0);
-//        delayMs(1);
-//        LCDWriteHere(displayChars.characters);
-
-        if ((distance == 0) && done == 1) {
-            echoCalc();
-        }
-//        updateTime();
-//        if (eventDue(&echo) && done == 0 && INTCON3bits.INT1IE == 0){
-//            INTCON3bits.INT1IF = 0;
-//            INTCON3bits.INT1IE = 1;
-//        }
-
+    if (samples <= 10 && samples > 0) {
+        USValues.sampleSize = samples;
     }
 }
 
+/**
+ * @brief [sets up the US fire frequency]
+ * cannot be set faster than 12 Hz
+ */
+void setUSFrequency (unsigned char frequency){
+    if (frequency > 1 && frequency < 13) {
+        USValues.freq_ms = 1000/frequency;
+    }
+}
+
+/**
+ * @brief Tests the current state of the US
+ * @details Call this function to test what
+ *          the state is and what needs to
+ *          be done. The If statements:
+ *      1)  Turns on the CCP2 if the noise threshold
+ *          has been passed.
+ *      2)  If an echo has not been recieved by then
+ *          it is out of range,calls calc passing 0
+ *      3)  Echo recieved, calls calc passing captured
+ *          cycle value
+ */
+void testUSState (void){
+    if (TMR3H > 0x07 && USFlags.fireStatus) {  ///< change to 0x0D
+        CCP2CON = 0x05; ///<capture mode, every rising edge
+    }
+    if (TMR3H > 0x3B && USFlags.fireStatus) {  ///<change to 0x58
+        UScapturedValue[USArrayPosition] = 0x00;
+        echoCalc();
+        CCP2CON = 0x00;                         ///<turn of ccp since no value is coming
+        USFlags.fireStatus = 0;                       ///<reset status
+    }
+
+    if (PIR2bits.CCP2IF && USFlags.fireStatus) {
+        PIR2bits.CCP2IF = 0;
+        UScapturedValue[USArrayPosition] = ReadTimer3();    ///<store captured time into array
+        echoCalc();
+        CCP2CON = 0x00;
+        USFlags.fireStatus = 0;
+    }
+}
+
+/**
+ * @brief Call this function to start an echo
+ *        transmission.
+ * @details Clears timer 3, sets initpin high
+ *          and sets the time tag for next echo
+ *          to be in 80 ms
+ */
 void fireEcho (void) {
     INITPIN = 0;
-    done = 0;
-    distance = 0;
-    milliTime = 0;
+    PIR2bits.CCP2IF = 0;
+    USFlags.fireStatus = 1;               ///<change status to be waiting for echos
     updateTime();
-    TMR0StartSubMilli = ReadTimer0();
-    storeCurrentTime(&echoStartTime);
-    storeCurrentTime(&echo);
-    INITPIN = 1;                    //Start transmitting echos*/                 
-    setTimeTag(2,&echo);
+    storeCurrentTime(&echoCanFire);
+    setTimeTag(&echoCanFire,USValues.freq_ms);    ///<time tag for when next echo can be sent
+    TMR3L = 0;
+    TMR3H = 0;
+    INITPIN = 1;                    ///<Start transmitting echos*/
 }
 
+/**
+ * @brief This function deals with data processing from US input
+ * @details it updates USDistance to contain the latest moving average
+ *          calculations. Any value that is above or below the min/max range
+ *          is treated as void,and given a value of 0.
+ *
+ */
 void echoCalc (void) {
-    
-//    INTCONbits.INT0IE = 0;
-    INTCON3bits.INT1IE = 0;
-    TMR0StartMilli = echoStartTime.milliseconds;
-    TMR0EndMilli = echoEndTime.milliseconds;
-    if (TMR0StartMilli > TMR0EndMilli) { ///<checking for overflows
-        TMR0EndMilli = TMR0EndMilli + 0x3E8; ///<adds 1000 to the end so calculation still works
+    unsigned char i;
+    unsigned char voidUSData;
+    unsigned int  milliTime;
+    unsigned int  subMilliTime;
+    unsigned int tempDistance;
+    USValues.distance = 0;
+    voidUSData = 0;
+    if (UScapturedValue[USArrayPosition] != 0) {
+        milliTime = UScapturedValue[USArrayPosition]/1000;                      ///<divides into millisec           //divide by 2500
+        subMilliTime = (UScapturedValue[USArrayPosition] - milliTime*1000)/50;  ///<divides into millisec/20        //multiply by 2500
+        if ((subMilliTime*50 + 25) < (UScapturedValue[USArrayPosition] - milliTime*1000)) {                         //multiply by 2500
+            subMilliTime = subMilliTime + 1;
+        }
+        UScapturedValue[USArrayPosition] = milliTime*distPerMs/10 + subMilliTime*distPerSubMs/10;   ///<gives a distance in cm (remove the /10 for mm)
     }
-    milliTime = TMR0EndMilli - TMR0StartMilli; ///<gives the time taken in milliseconds
-    if (TMR0StartSubMilli >= TMR0EndSubMilli) { ///<case where the start is a higher value then the end
-        subMilliTime = 0xFF - (TMR0StartSubMilli - TMR0EndSubMilli); ///< gives the correct value without going above FF
+    i = 0;
+    while (i < USValues.sampleSize) {  ///<checks for void values
+        tempDistance = UScapturedValue[i];
+        if (tempDistance < USValues.minRange/10 || tempDistance > USValues.maxRange/10) {       ///<remove /10 for mm
+            tempDistance = 0;
+            voidUSData++;
+        }
+        USValues.distance += tempDistance;
+        i++;
     }
-    else {
-        subMilliTime = TMR0EndSubMilli - TMR0StartSubMilli; ///<case where the end is a higher value then the start
+
+    if (voidUSData != USValues.sampleSize) {
+        USValues.distance = USValues.distance/(USValues.sampleSize-voidUSData); ///<average distance values
     }
-    i = subMilliTime >> 5;                ///<bit shift to store 3 most sig bits
-    distance = i*distPerSubMs;                         ///<simplification of distance from submilliseconds
-    distance = distance + milliTime*distPerMs;     ///<simplification of distance from milliseconds
-    INITPIN = 0;
-    done = 2;
-    //storeCurrentTime(echo);
-    //setTimeTag(900,echo);
+    USArrayPosition++;
+    if (USArrayPosition = USValues.sampleSize) {
+        USArrayPosition = 0; ///<reset poisition if over buffer
+    }
+    USFlags.distanceReady = 1;
 }
 
-#pragma interrupt highPriorityIsr
-void highPriorityIsr (void) {
-    updateTime();
-    if (INTCON3bits.INT1IF && INTCON3bits.INT1IE && eventDue(&echo)) {     ///<When echo is recieved as high
-        updateTime();
-        TMR0EndSubMilli = ReadTimer0();            ///<check submilli interval
-        storeCurrentTime(&echoEndTime);             ///<store final time
-        done = 1;                                    ///<show it is ready for calc
-        INTCON3bits.INT1IF = 0;
-    }
-     if( INTCONbits.TMR0IF ){
-        time.updatesRequired++;	///< Flag another update for the main
-        INTCONbits.TMR0IF = 0; ///< Clear the interrupt flag
-    }
-}
-
-#pragma config OSC = HS
-#pragma config PWRT = ON
-#pragma config WDT = OFF, WDTPS = 1
-#pragma config LVP = OFF
-#pragma config DEBUG = OFF
-#pragma config CP0 = OFF,CP1 = OFF,CP2 = OFF,CP3 = OFF,CPB = OFF,CPD = OFF
-#pragma config WRT0 = OFF,WRT1 = OFF,WRT2 = OFF,WRT3 = OFF,WRTB = OFF,WRTC = OFF,WRTD = OFF
-#pragma config EBTR0 = OFF,EBTR1 = OFF,EBTR2 = OFF,EBTR3 = OFF,EBTRB = OFF
--- deathStarTracker/ultrasound.c
+++ deathStarTracker/ultrasound.c
@@ -17,25 +17,25 @@
 
 
 /// The external variables are all declared here
-unsigned int  milliTime = 0;
-unsigned int  subMilliTime = 0;
-unsigned char USStatus = 0;
+
+
 unsigned char USArrayPosition = 0;
-volatile unsigned char USOverF = 0;
-unsigned int distance = 0;
-unsigned int tempDistance = 0;
-unsigned int speed = 0;
+unsigned int speedOfSound = 0;
 unsigned int distPerMs = 0;
 unsigned int distPerSubMs = 0;
 unsigned int UScapturedValue[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-timeTag echo = {0,0,0,0};
+timeTag echoCanFire = {0,0,0,0};
+USFlagType USFlags = {0, 0};
+USValueType USValues = {0,0,0,0,0};
+
 
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief [sets up the US]
+ * @details [configures timer 3 and sets initial status of
+ *			 US variables]
  */
-void setup(void) {
+void USSetup(void) {
     INITPIN = 0;
     ECHOTRIS = 1;                   ///<Input for Echo
     INITTRIS = 0;                   ///<Output for INIT
@@ -51,64 +51,150 @@ void setup(void) {
     T3CONbits.T3CKPS0 = 0; ///<prescaler is 1:1
     T3CONbits.TMR3ON = 1;
 
-    PIR2bits.TMR3IF = 0;
-    PIE2bits.TMR3IE = 0;
-
-    milliTime = 0;
-    subMilliTime = 0;
-    distance = 0;
+    USValues.distance = 0;                         ///<holds current value of calc distance
     setupRealTimeTimer();
-    speed = 34000;
-    distPerMs = speed/200;
-    distPerSubMs = speed/4000;
-    USOverF = 0;
-    USStatus = 0;
+    speedOfSound = 34000;                          ///<speed in cm/s
+    distPerMs = speedOfSound/200;                  ///<divides by 2 for travel, 100 to be mm/ms
+    distPerSubMs = speedOfSound/4000;              ///<divides by 20 times to get 20 subsections
+    USFlags.fireStatus = 0;                       ///<status of 0 means nothing is occuring, 1 means echo is incoming and has not been calculated yet
+    USFlags.distanceReady = 0;                    ///<whether the new distance has been read
+    USValues.sampleSize = 2;                       ///<starting values
+    USValues.minRange = 450;
+    USValues.maxRange = 2100;
+    USValues.freq_ms = 80;
     USArrayPosition = 0;
 }
 
+/**
+ * @brief [sets up the US Min Distance]
+ */
+void setUSMin (unsigned int min){
+    USValues.minRange = min;
+}
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief [sets up the US Max Distance]
+ */
+void setUSMax (unsigned int max){
+    USValues.maxRange = max;
+}
+
+/**
+ * @brief [sets up the US Samples per Average]
+ */
+void setUSSamplePerEstimate (unsigned char samples){
+    unsigned char i = 0;
+    while (i < 10) {
+        UScapturedValue[i] = 0;
+        i++;
+    }
+    if (samples <= 10 && samples > 0) {
+        USValues.sampleSize = samples;
+    }
+}
+
+/**
+ * @brief [sets up the US fire frequency]
+ * cannot be set faster than 12 Hz
+ */
+void setUSFrequency (unsigned char frequency){
+    if (frequency > 1 && frequency < 13) {
+        USValues.freq_ms = 1000/frequency;
+    }
+}
+
+/**
+ * @brief Tests the current state of the US
+ * @details Call this function to test what
+ *          the state is and what needs to
+ *          be done. The If statements:
+ *      1)  Turns on the CCP2 if the noise threshold
+ *          has been passed.
+ *      2)  If an echo has not been recieved by then
+ *          it is out of range,calls calc passing 0
+ *      3)  Echo recieved, calls calc passing captured
+ *          cycle value
+ */
+void testUSState (void){
+    if (TMR3H > 0x07 && USFlags.fireStatus) {  ///< change to 0x0D
+        CCP2CON = 0x05; ///<capture mode, every rising edge
+    }
+    if (TMR3H > 0x3B && USFlags.fireStatus) {  ///<change to 0x58
+        UScapturedValue[USArrayPosition] = 0x00;
+        echoCalc();
+        CCP2CON = 0x00;                         ///<turn of ccp since no value is coming
+        USFlags.fireStatus = 0;                       ///<reset status
+    }
+
+    if (PIR2bits.CCP2IF && USFlags.fireStatus) {
+        PIR2bits.CCP2IF = 0;
+        UScapturedValue[USArrayPosition] = ReadTimer3();    ///<store captured time into array
+        echoCalc();
+        CCP2CON = 0x00;
+        USFlags.fireStatus = 0;
+    }
+}
+
+/**
+ * @brief Call this function to start an echo
+ *        transmission.
+ * @details Clears timer 3, sets initpin high
+ *          and sets the time tag for next echo
+ *          to be in 80 ms
  */
 void fireEcho (void) {
     INITPIN = 0;
-    CCP2CON = 0x00;
-    USStatus = 1;
-    distance = 0;
-    milliTime = 0;
+    PIR2bits.CCP2IF = 0;
+    USFlags.fireStatus = 1;               ///<change status to be waiting for echos
+    updateTime();
+    storeCurrentTime(&echoCanFire);
+    setTimeTag(&echoCanFire,USValues.freq_ms);    ///<time tag for when next echo can be sent
     TMR3L = 0;
     TMR3H = 0;
-    USOverF = 0;
-    PIR2bits.TMR3IF = 0;
-    PIE2bits.TMR3IE = 1;
-    INITPIN = 1;                    //Start transmitting echos*/
+    INITPIN = 1;                    ///<Start transmitting echos*/
 }
-
-
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief This function deals with data processing from US input
+ * @details it updates USDistance to contain the latest moving average
+ *          calculations. Any value that is above or below the min/max range
+ *          is treated as void,and given a value of 0.
+ *
  */
 void echoCalc (void) {
-    unsigned char i = 0;
-    distance = 0;
-    while (i < USArrayPosition) {
-        milliTime = UScapturedValue[i]/1000;
-        subMilliTime = (UScapturedValue[i] - milliTime*1000)/50;
-        if ((subMilliTime*50 + 25) < (UScapturedValue[i] - milliTime*1000)) {
+    unsigned char i;
+    unsigned char voidUSData;
+    unsigned int  milliTime;
+    unsigned int  subMilliTime;
+    unsigned int tempDistance;
+    USValues.distance = 0;
+    voidUSData = 0;
+    if (UScapturedValue[USArrayPosition] != 0) {
+        milliTime = UScapturedValue[USArrayPosition]/1000;                      ///<divides into millisec           //divide by 2500
+        subMilliTime = (UScapturedValue[USArrayPosition] - milliTime*1000)/50;  ///<divides into millisec/20        //multiply by 2500
+        if ((subMilliTime*50 + 25) < (UScapturedValue[USArrayPosition] - milliTime*1000)) {                         //multiply by 2500
             subMilliTime = subMilliTime + 1;
         }
-        tempDistance = milliTime*distPerMs/10 + subMilliTime*distPerSubMs/10;
-        if (tempDistance < 45 || tempDistance > 210) {
-            tempDistance = 0;
-        }
-        i++;
-        distance += tempDistance;
+        UScapturedValue[USArrayPosition] = milliTime*distPerMs/10 + subMilliTime*distPerSubMs/10;   ///<gives a distance in cm (remove the /10 for mm)
     }
-    distance = distance/USArrayPosition;
-    USArrayPosition = 0;
-}
+    i = 0;
+    while (i < USValues.sampleSize) {  ///<checks for void values
+        tempDistance = UScapturedValue[i];
+        if (tempDistance < USValues.minRange/10 || tempDistance > USValues.maxRange/10) {       ///<remove /10 for mm
+            tempDistance = 0;
+            voidUSData++;
+        }
+        USValues.distance += tempDistance;
+        i++;
+    }
 
+    if (voidUSData != USValues.sampleSize) {
+        USValues.distance = USValues.distance/(USValues.sampleSize-voidUSData); ///<average distance values
+    }
+    USArrayPosition++;
+    if (USArrayPosition = USValues.sampleSize) {
+        USArrayPosition = 0; ///<reset poisition if over buffer
+    }
+    USFlags.distanceReady = 1;
+}
 
