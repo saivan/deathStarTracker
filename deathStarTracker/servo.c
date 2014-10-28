7f45885b5747f0ae6ead4414b7c6e5d00d5240b2
saivan <saivan@me.com>
2014-10-29 00:54:35 +1100

    The servos now have a calibration function working too.

--- deathStarTracker/servo.c
+++ deathStarTracker/servo.c
@@ -19,6 +19,10 @@ unsigned int azimuthMax = 1800;
 unsigned int elevationMin = 0;
 unsigned int elevationMax = 900;
 
+unsigned int zeroAzimuth = (SERVO_CCP_MIN) + 4 - 1100;
+unsigned int zeroElevation = (SERVO_CCP_MIN) + 4 - 1000;
+
+
 /**
  * @brief Setup the servos to start moving when powered
  * Given a particular azimuth and elevation, the servos will start continuously
@@ -46,6 +50,29 @@ void setupServos( void ){
 
 
 /**
+ * @brief Calibrates the zero position of the azimuth servo
+ * The new zero position is simply defined as 
+ *      $ \text{A_\text{zero}} = {5\cdot \tau_\text{input} \over 2} $
+ * @param inputUSDuration Time in microseconds to define the zero position
+ */
+void calibrateAzimuthOffset( unsigned int inputUsDuration ){
+    zeroAzimuth = (5*inputUSDuration)>>1;
+}
+
+
+/**
+ * @brief Calibrates the zero position of the elevation servo
+ * The new zero position is simply defined as 
+ *      $ \text{E_\text{zero}} = {5\cdot \tau_\text{input} \over 2} $
+ * @param inputUSDuration Time in microseconds to define the zero position
+ */
+void calibrateElevationOffset( unsigned int inputUsDuration ){
+    zeroElevation = (5*inputUSDuration)>>1;   
+}
+
+
+
+/**
  * @brief Calculates the CCP values to reach a particular azimuth and elevation
  * Uses the inputs to calculate the desired azimuth and elevation on times for the CCP module
  * to use during it's execution. 
@@ -58,12 +85,12 @@ void updateCCPServoAngle( unsigned int azimuthAngle, unsigned int elevationAngle
     /// Calculate the CCPTime by using $ \text{time} = {2000\over 1800}\theta + 2000 $
 
     if ( ( azimuthAngle <= azimuthMax ) &&  ( azimuthAngle >= azimuthMin)  ){    
-        azimuthServoOnTime = ((5*azimuthAngle)>>1) + SERVO_AZIMUTH_YINT;            ///< Calculate the new azimuth servo On time
+        azimuthServoOnTime = ((5*azimuthAngle)>>1) + zeroAzimuth;            ///< Calculate the new azimuth servo On time
         currentAzimuth = azimuthAngle;                                              ///< Store the new Azimuth
     }
 
     if( ( elevationAngle <= elevationMax ) && ( elevationAngle >= elevationMin) ){
-        elevationServoOnTime = ((5*elevationAngle)>>1) + SERVO_ELEVATION_YINT;      ///< Calculate the New elevation servo On time
+        elevationServoOnTime = ((5*elevationAngle)>>1) + zeroElevation;      ///< Calculate the New elevation servo On time
         currentElevation = elevationAngle;                                          ///< Store the new Elevation
     }    
         
--- deathStarTracker/stateFunctions.c
+++ deathStarTracker/stateFunctions.c
@@ -1,12 +1,12 @@
 
 
 
-#include "masterHeader.h"
+#include "system.h"
 
 
 void showTargetStatus(void)
 {
-    
+    //show what mode the main is in
 }
 
 void showTemperature(void)
@@ -16,52 +16,210 @@ void showTemperature(void)
 
 void azimSetAngle(void)
 {
+	//NOTE SOME MODE MUST BE SET THAT IS NOT SCANNING OR TRACKING
 
+	if(systemFlags.userInputStored)
+    {
+		if ((parsedNumber >= 0) && (parsedNumber <= 180))
+		{
+			updateCCPServoAngle(parsedNumber*10, currentElevation);
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else 
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
+	
 }
 
 void azimManual(void)
 {
+	//NOTE SOME MODE MUST BE SET THAT IS NOT SCANNING OR TRACKING
+	//MUST ALSO BE SET TO TRIGGER FROM BUTTON PRESSES
 
+	if (1) {   //if some positive button
+		updateCCPServoAngle(currentAzimuth+50, currentElevation);
+		printRomString(msgValueGood);
+		moveToParentNode();
+		systemFlags.updatePrompt = 1;
+	}
+	else 
+	{
+		updateCCPServoAngle(currentAzimuth-50, currentElevation);
+		printRomString(msgValueGood);
+		moveToParentNode();
+		systemFlags.updatePrompt = 1;
+	}
 }
 
 void elevSetAngle(void)
 {
+	//NOTE SOME MODE MUST BE SET THAT IS NOT SCANNING OR TRACKING
 
+	if(systemFlags.userInputStored)
+    {
+		if ((parsedNumber >= 0) && (parsedNumber <= 180))
+		{
+			updateCCPServoAngle(currentAzimuth, parsedNumber*10);
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else 
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void elevManual(void)
 {
+	//NOTE SOME MODE MUST BE SET THAT IS NOT SCANNING OR TRACKING
+	//MUST ALSO BE SET TO TRIGGER FROM BUTTON PRESSES
 
+	if (1) {   //if some positive button
+		updateCCPServoAngle(currentAzimuth, currentElevation+50);
+		printRomString(msgValueGood);
+		moveToParentNode();
+		systemFlags.updatePrompt = 1;
+	}
+	else 
+	{
+		updateCCPServoAngle(currentAzimuth, currentElevation-50);
+		printRomString(msgValueGood);
+		moveToParentNode();
+		systemFlags.updatePrompt = 1;
+	}
+	
 }
 
 void distMax(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+		if ((parsedNumber >= 45) && (parsedNumber <= 210))
+		{
+			setIRMax(parsedNumber);
+			setUSMax(parsedNumber);
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else 
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void distMin(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+		if ((parsedNumber >= 45) && (parsedNumber <= 210))
+		{
+			setIRMin(parsedNumber);
+			setUSMin(parsedNumber);
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else 
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void azimMax(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 180))
+        {
+			azimuthMax = parsedNumber*10;   //converts degrees to tens of degrees
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void azimMin(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 180))
+        {
+			azimuthMin = parsedNumber*10;   //converts degrees to tens of degrees
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void elevMax(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 90))
+        {
+			elevationMax = parsedNumber*10;   //converts degrees to tens of degrees
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void elevMin(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 90))
+        {
+			elevationMin = parsedNumber*10;   //converts degrees to tens of degrees
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 
@@ -82,59 +240,124 @@ void caliElev(void)
 
 void usSamplePerEst(void)
 {
+	unsigned char i = 0;
+
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 10))
+        {
+			while (i < 10) 
+			{
+				UScapturedValue[i] = 0;
+				i++;
+			}
+			USValues.sampleSize = parsedNumber;
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void usSampleRate(void)
 {
-//    if(systemFlags.userInputStored)
-//    {
-//        /// User inputs 2 to 13 Hz
-//        if((parsedNumber >= 2) && (parsedNumber <= 13))
-//        {
-//            USValues.freq_ms = 1000/parsedNumber;
-//
-//            printRomString(msgValueGood);
-//            moveToParentNode();
-//            systemFlags.updatePrompt = 1;
-//        }
-//        else
-//        {
-//            systemFlags.userInputStored = 0;
-//            printRomString(msgValueBad);
-//            systemFlags.updatePrompt = 1;
-//        }
-//    }
+    if(systemFlags.userInputStored)
+    {
+        /// User inputs 2 to 13 Hz
+        if((parsedNumber >= 2) && (parsedNumber <= 12))
+        {
+            USValues.freq_ms = 1000/parsedNumber;
+
+            printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+        }
+        else
+        {
+            systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+        }
+    }
 }
 
 void irSamplePerEst(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber > 0) && (parsedNumber <= 10))
+        {
+			setIRSamplesPerEstimate(parsedNumber);
+			printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+		}
+		else
+		{
+			systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+		}
+	}
 }
 
 void irSampleRate(void)
 {
-
+	if(systemFlags.userInputStored)
+    {
+        if((parsedNumber >= 200)
+        {
+			setIRSampleRate(parsedNumber);
+            printRomString(msgValueGood);
+            moveToParentNode();
+            systemFlags.updatePrompt = 1;
+        }
+        else
+        {
+            systemFlags.userInputStored = 0;
+            printRomString(msgValueBad);
+            systemFlags.updatePrompt = 1;
+        }
+    }
 }
 
 void showRaw(void)
 {
-//    int * rawDataPtr = USCapturedRaw;
-//
-//    if(*rawDataPtr++)
-//    {
-//        intToDisplay(*rawDataPtr, 0);
-//        printRamString(displayChars.characters);
-//
-//        printRomString(msgSpaces);
-//    }
-//    else
-//    {
-//        printRomString(msgCR);
-//    }
-//    handleTransmission;
+    
+    intToDisplay(USCapturedRaw[0], 0);
+    printRamString(displayChars.characters);
+
+	printRomString(msgSpaces);
+
+	intToDisplay(getIRRaw(), 0);
+    printRamString(displayChars.characters);
+
+    printRomString(msgCR);
+    handleTransmission;
 }
 
 void showStat(void)
 {
+	unsigned char i = 0;
+    unsigned int maxVariance = 0;
+    unsigned int variance = 0;
+    while (i < USValues.currentSampleSize){
+        variance = ((USCapturedRaw[i])<<1)>>1;
+        if ( variance > maxVariance){
+            maxVariance = variance;
+        }
+    }
+	intToDisplay(maxVariance, 0);
+    printRamString(displayChars.characters);
 
+	printRomString(msgSpaces);
+
+	intToDisplay(getIRVariance(), 0);
+    printRamString(displayChars.characters);
 }
